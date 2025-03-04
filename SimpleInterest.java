4a7223aa43413127ecc8917f6db789a8fc4173d9
madhav1602 <madhavaggarwal580@gmail.com>
2025-03-04 13:34:16 +0530

    [Madhav] Add: finding simple interest

--- SimpleInterest.java
+++ SimpleInterest.java
@@ -1,7 +1,25 @@
- 
+import java.util.Scanner;
 class SimpleInterest {
-	public static void main(String[] args){
 
-	
-  }
+        public static void main(String[] args){
+		Scanner sc= new Scanner(System.in);
+			
+		// Take user input
+		Double principal=sc.nextDouble();
+		Double rate=sc.nextDouble();
+		Double time=sc.nextDouble();
+		
+		// Call the method
+		Double simpleInterest= simpleInterest(principal,rate,time);
+			
+		// Print the result
+		System.out.println("The Simple Interest is " + simpleInterest + " for Principal " + principal + ", Rate of Interest " + rate + " and Time " + time);
+
+	}
+
+	// Static method to calculate Simple Interest
+	public static Double simpleInterest(Double principal, Double rate, Double time){
+		Double result=(principal * rate * time) / 100;
+		return result;
+	}
 }

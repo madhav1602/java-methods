83efdc8e24a345e605e43fe635b6c8db00986114
madhav1602 <madhavaggarwal580@gmail.com>
2025-03-05 15:07:29 +0530

    [Madhav] Add: doing Matrix Operations

---
+++ MatrixOperations.java
@@ -0,0 +1,80 @@
+import java.util.*;
+
+public class MatrixOperations {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number of rows: ");
+        int rows = sc.nextInt();
+        System.out.print("Enter number of columns: ");
+        int cols = sc.nextInt();
+
+        int[][] matrix1 = generateMatrix(rows, cols);
+        int[][] matrix2 = generateMatrix(rows, cols);
+        
+        System.out.println("Matrix 1:");
+        displayMatrix(matrix1);
+        System.out.println("Matrix 2:");
+        displayMatrix(matrix2);
+        
+        System.out.println("Addition:");
+        displayMatrix(addMatrices(matrix1, matrix2));
+        
+        System.out.println("Subtraction:");
+        displayMatrix(subtractMatrices(matrix1, matrix2));
+        
+        if (rows == cols) {
+            System.out.println("Determinant of Matrix 1: " + determinant(matrix1));
+            System.out.println("Determinant of Matrix 2: " + determinant(matrix2));
+        }
+    }
+    
+    public static int[][] generateMatrix(int rows, int cols) {
+        int[][] matrix = new int[rows][cols];
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                matrix[i][j] = (int) (Math.random() * 10);
+            }
+        }
+        return matrix;
+    }
+    
+    public static int[][] addMatrices(int[][] m1, int[][] m2) {
+        int[][] result = new int[m1.length][m1[0].length];
+        for (int i = 0; i < m1.length; i++) {
+            for (int j = 0; j < m1[0].length; j++) {
+                result[i][j] = m1[i][j] + m2[i][j];
+            }
+        }
+        return result;
+    }
+    
+    public static int[][] subtractMatrices(int[][] m1, int[][] m2) {
+        int[][] result = new int[m1.length][m1[0].length];
+        for (int i = 0; i < m1.length; i++) {
+            for (int j = 0; j < m1[0].length; j++) {
+                result[i][j] = m1[i][j] - m2[i][j];
+            }
+        }
+        return result;
+    }
+    
+    public static int determinant(int[][] matrix) {
+        if (matrix.length == 2) {
+            return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0];
+        } else if (matrix.length == 3) {
+            return matrix[0][0] * (matrix[1][1] * matrix[2][2] - matrix[1][2] * matrix[2][1]) -
+                   matrix[0][1] * (matrix[1][0] * matrix[2][2] - matrix[1][2] * matrix[2][0]) +
+                   matrix[0][2] * (matrix[1][0] * matrix[2][1] - matrix[1][1] * matrix[2][0]);
+        }
+        return 0;
+    }
+    
+    public static void displayMatrix(int[][] matrix) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
